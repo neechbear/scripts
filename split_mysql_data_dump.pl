e708ffb75a2a67215a1d96a7fb8e2e76b781d104
Nicola Worthington <nicolaw@tfb.net>
2007-01-16 13:43:09 +0000

    git-svn-id: file:///var/tmp/data/svnroot/scripts@912 610ec7d1-1c12-4784-ad90-13d12571cf29

---
+++ split_mysql_data_dump.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+
+use 5.6.1;
+use strict;
+use warnings;
+
+my $file = $ARGV[0] || '';
+die "Please specify a MySQL dump filename.\n" unless $file && -f $file;
+
+my @header;
+my $table = '';
+my $lastline = '';
+my $out;
+
+open(FH,'<',$file) || die "Unable to open file '$file': $!\n";
+while (local $_ = <FH>) {
+	if (!$table && /^--\s*Dumping data for table/i && $lastline =~ /^--\s*$/) {
+		pop @header;
+		if (/^--\s*Dumping data for table \`(\S+)?\`/) { $table = $1; }
+		$out = open_sql_file("$file.$table.sql",$out);
+
+	} elsif (!$table) {
+		push @header, $_;
+
+	} elsif (/^--\s*Dumping data for table \`(\S+)?\`/) {
+		$table = $1;
+		$out = open_sql_file("$file.$table.sql",$out);
+		print $out join('',@header);
+		print $out $lastline;
+		print $out $_;
+
+	} else {
+		print $out $_;
+	}
+
+	$lastline = $_;
+}
+close(FH) || warn "Unable to close file '$file': $!\n";
+close($out) if defined $out;
+
+exit;
+
+sub open_sql_file {
+	my ($file,$fh) = @_;
+	close($fh) if defined $fh;
+	die "$file already exists; aborting!\n" if -e $file;
+	open($fh,'>',$file) || die "Unable to open file '$file': $!\n";
+	return $fh;
+}
+
