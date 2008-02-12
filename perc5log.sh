c3b3ae43144222bddbf0db7e69881a85e58c9be1
Nicola Worthington <nicolaw@tfb.net>
2008-02-12 11:31:11 +0000

    git-svn-id: file:///var/tmp/data/svnroot/scripts@1105 610ec7d1-1c12-4784-ad90-13d12571cf29

--- perc5log.sh
+++ perc5log.sh
@@ -7,36 +7,50 @@ dmidecode | grep -A 9 "System Information" | egrep "(Manufacturer|Product Name|S
 
 echo >> $LOGFILE
 echo >> $LOGFILE
+echo "-------------------------------------------------------------------------------" >> $LOGFILE
 echo "- Controller information" >> $LOGFILE
-
 MegaCli -AdpAllInfo -aALL >> $LOGFILE
 MegaCli -CfgDsply -aALL >> $LOGFILE
-MegaCli -AdpEventLog -GetEvents -f events.log -aALL && cat events.log >> $LOGFILE
 
 echo >> $LOGFILE
 echo >> $LOGFILE
+echo "-------------------------------------------------------------------------------" >> $LOGFILE
 echo "- Enclosure information" >> $LOGFILE
-
 MegaCli -EncInfo -aALL >> $LOGFILE
 
+echo >> $LOGFILE
+echo >> $LOGFILE
+echo "-------------------------------------------------------------------------------" >> $LOGFILE
 echo "- Virtual drive information" >> $LOGFILE
-echo >> $LOGFILE
-echo >> $LOGFILE
-
 MegaCli -LDInfo -Lall -aALL >> $LOGFILE
 
 echo >> $LOGFILE
 echo >> $LOGFILE
+echo "-------------------------------------------------------------------------------" >> $LOGFILE
 echo "- Physical drive information" >> $LOGFILE
-
 MegaCli -PDList -aALL >> $LOGFILE
-#MegaCli -PDInfo -PhysDrv [E:S] -aALL >> $LOGFILE
+
+for i in `MegaCli -PDList -aALL  | perl -ne 'if (/^(Enclosure|Slot)\s+Number:\s+([0-9]{1,2})\s*$/) { if (substr($1,0,1) eq "E") { $E = $2; } else { print "[$E:$2]\n"; } }'`
+do
+	echo >> $LOGFILE
+	echo >> $LOGFILE
+	echo "-------------------------------------------------------------------------------" >> $LOGFILE
+	echo "- Physical drive information $i" >> $LOGFILE
+	MegaCli -PDInfo -PhysDrv $i -aAll >> $LOGFILE
+done
 
 echo >> $LOGFILE
 echo >> $LOGFILE
+echo "-------------------------------------------------------------------------------" >> $LOGFILE
 echo "- Battery backup information" >> $LOGFILE
-
 MegaCli -AdpBbuCmd -aALL >> $LOGFILE
+
+
+echo >> $LOGFILE
+echo >> $LOGFILE
+echo "-------------------------------------------------------------------------------" >> $LOGFILE
+echo "- Controller event log" >> $LOGFILE
+MegaCli -AdpEventLog -GetEvents -f events.log -aALL && cat events.log >> $LOGFILE
 
 echo "Created $LOGFILE."
 
